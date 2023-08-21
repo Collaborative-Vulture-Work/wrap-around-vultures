1a940fe25f49944eab170a8555e9cfd3be67bfdb
Ryan Nguyen <ryannguyen6392@gmail.com>
2023-08-20 21:19:30 -0700

    Second conveyor belt iteration; dates now properly shift and loop around simulation date starts at 8/13 00:00 with maxdate at 11/21 08:10 original_edgelist is an edgelist of the original simulation data rotated_edgelist is an edgelist of rotated data with set seed
    Despite the current lack of times the conveyor belt is run,
    the original edgelist and rotated edgelist have similar numbers
    of observations, which looks promising for a sufficient null model

--- datastream_shifter.R
+++ datastream_shifter.R
@@ -5,58 +5,63 @@ library(spatsoc)
 
 SAMPLING_INTERVAL <- 10 # "minutes", from matlab code; 10 minutes per timestep with 50 timesteps gives about 8hrs of data
 
+# loops a single posix around min and max
 loop_days <- function(data, min, max, shift){
   shifted <- data + shift
-  if(shifted > max){
-    shifted <- min + (shift - as.period(floor(difftime(max, data, units="days"))))
+  if(shifted > max){ 
+    shifted <- min + shift - as.period(difftime(max, data, units="days")) - difftime(ceiling_date(max, unit="day"), max, units="hours") + hours(1)
   }
   shifted
 }
 
+# gets network graph
+get_edgelist <- function(data){
+  timegroup_data <- data.frame(data)
+  data.table::setDT(timegroup_data)
+  timegroup_data <- spatsoc::group_times(timegroup_data, datetime = "datetime", threshold = "10 minutes")
+  spatsoc::edge_dist(timegroup_data, threshold = 14, id = "indiv", coords = c('x','y'), timegroup = "timegroup", returnDist = FALSE, fillNA = FALSE)
+}
+
+# loads data and changes day and step to posix
 load_data <- function(){
   load('xyFromSimulationForSNanalysis_5000_60_70_7_0_.RData')
   simulation_data <- XYind_log2
-  start_time <- as.POSIXct("2023-08-12 00:00")  # note simulation data starts on day 1 so the mindate will be 8-13 00:10
+  start_time <- as.POSIXct("2023-08-11 23:50")  # note simulation data starts on day 1 step 1 so the mindate will be 8-13 00:00
   simulation_data <- simulation_data %>%
     dplyr::mutate(datetime = start_time + lubridate::days(Day) + lubridate::minutes(StepInDay * SAMPLING_INTERVAL)) %>%
     dplyr::select(indiv, x, y, datetime)
-  # timegroup_data <- data.frame(simulation_data)
-  # data.table::setDT(timegroup_data)
-  # timegroup_data <- spatsoc::group_times(timegroup_data, datetime = "datetime", threshold = "10 minutes")
-  # original_edges <- spatsoc::edge_dist(timegroup_data, threshold = 14, id = "indiv", coords = c('x','y'), timegroup = "timegroup", returnDist = FALSE, fillNA = FALSE)
-  
-  
-  ## graph network graph from simulation
-  
-  ## run conveyor / randomizations
-  
+  simulation_data
+}
+
+rotate_data <- function(data){
   # shifting forward n same as shifting forward and back n/2 ?
   
   ## SET SEED
-  # set.seed(2023)
+  set.seed(2023)
   
-  sampled_shift <- simulation_data %>%
+  sampled_shift <- data %>%
     dplyr::group_by(indiv) %>%
     dplyr::summarise(mindate = min(datetime), maxdate = max(datetime), sampledShift = lubridate::days(sample(1:floor(difftime(max(datetime), min(datetime), units="days") - 1), 1)))
   
-  simulation_data <- dplyr::inner_join(simulation_data, sampled_shift, by = dplyr::join_by(indiv))
+  data <- dplyr::inner_join(data, sampled_shift, by = dplyr::join_by(indiv))
   
   loop_days <- Vectorize(loop_days)
   
-  simulation_data <- simulation_data %>%
+  data <- data %>%
     dplyr::mutate(datetime = as.POSIXct(loop_days(datetime, mindate, maxdate, sampledShift)), .by = indiv) %>%
-    # dplyr::select(indiv, x, y, datetime) %>%
-    dplyr::filter(indiv == 1)
-  
-  simulation_data
-  ## use spatsoc to get network graph again                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
-  
-  # shifted_timegroup_data <- data.frame(simulation_data)
-  # data.table::setDT(shifted_timegroup_data)
-  # shifted_timegroup_data <- spatsoc::group_times(shifted_timegroup_data, datetime = "datetime", threshold = "10 minutes")
-  # shifted_edges <- spatsoc::edge_dist(shifted_timegroup_data, threshold = 14, id = "indiv", coords = c('x','y'), timegroup = "timegroup", returnDist = FALSE, fillNA = FALSE)
-  
+    dplyr::select(indiv, x, y, datetime)
+  data
 }
 
-data <- load_data()
+main <- function(){
+  sim_data <- load_data()
+  original_edgelist <- get_edgelist(sim_data)
+  # rotated_data <- rotate_data(sim_data)
+  load("rotated_data.Rdata")
+  rotated_edgelist <- get_edgelist(rotated_data)
+  save(original_edgelist, file="original_edgelist.Rdata")
+  save(rotated_edgelist, file="rotated_edgelist.Rdata")
+}
+
+main()
 
