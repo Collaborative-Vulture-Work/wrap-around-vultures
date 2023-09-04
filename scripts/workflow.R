b326cdeaaea7c0122047fddd592064cf3deb4b25
Kaija Gahm <kgahm@ucla.edu>
2023-09-04 13:30:12 -0700

    Full code for sociable and non-sociable agents, plots for degree and strength

--- scripts/workflow.R
+++ scripts/workflow.R
@@ -5,71 +5,118 @@ library(tidyverse) # for data wrangling and plotting
 library(spatsoc) # to implement the trajectory randomization method as described in Orr's paper
 
 # 1. Run the simulation to obtain simulated data --------------------------
-# RANDOM
-sim_data <- simulateAgents(N_indv = 10, DaysToSimulate = 10, Kappa_ind = 3, quiet = T, ToPlot = 0)
-str(sim_data, 1) # we end up with a list: file names, and the things to save.
+# NON-SOCIABLE
+sim_data_ns <- simulateAgents(N_indv = 10, DaysToSimulate = 10, Kappa_ind = 3, quiet = T, ToPlot = 0, Social_Pecrt_rng = 0)
+str(sim_data_ns, 1) # we end up with a list: file names, and the things to save.
 # Save R data:
-save(sim_data, file = "data/sim_data.Rda") # XXXK come back to this
+save(sim_data_ns, file = "data/sim_data_ns.Rda") # XXXK come back to this
 # Save Matlab data: (note: this can take a really long time!)
-# R.matlab::writeMat(con = paste0("data/", sim_data$matlabName), XY = sim_data$XY, HRCntXY = sim_data$HRCntXY)
+# R.matlab::writeMat(con = paste0("data/", sim_data_ns$matlabName), XY = sim_data_ns$XY, HRCntXY = sim_data_ns$HRCntXY)
 
+# SOCIABLE
+sim_data_s <- simulateAgents(N_indv = 10, DaysToSimulate = 10, Kappa_ind = 3, quiet = T, ToPlot = 0, Social_Pecrt_rng = 2000)
+# Save R data:
+save(sim_data_s, file = "data/sim_data_s.Rda")
 
 # 2. Load the simulated data and extract the xy coords --------------------
-load("data/sim_data.Rda")
-simulation_data <- sim_data$XY # extract just the XY coords
-simulation_data <- fix_times(simulation_data)
+load("data/sim_data_ns.Rda")
+sd_ns <- sim_data_ns$XY # extract just the XY coords
+sd_ns <- fix_times(sd_ns)
+
+load("data/sim_data_s.Rda")
+sd_s <- sim_data_s$XY # extract just the XY coords
+sd_s <- fix_times(sd_s)
 
 # 3. Get permutation realizations -----------------------------------------
 n <- 100
 # CONVEYOR
-realizations_conveyor <- vector(mode = "list", length = n)
+## Conveyor: NON-SOCIABLE
+realizations_conveyor_ns <- vector(mode = "list", length = n)
 for(i in 1:n){
-  realizations_conveyor[[i]] <- rotate_data_table(data = simulation_data, idCol = "indiv", dateCol = "datetime")
+  realizations_conveyor_ns[[i]] <- rotate_data_table(data = sd_ns, idCol = "indiv", dateCol = "datetime")
 }
-realizations_conveyor <- map(realizations_conveyor, as.data.frame) # turn back to data frame
+realizations_conveyor_ns <- map(realizations_conveyor_ns, as.data.frame) # turn back to data frame
+
+## Conveor: SOCIABLE
+realizations_conveyor_s <- vector(mode = "list", length = n)
+for(i in 1:n){
+  realizations_conveyor_s[[i]] <- rotate_data_table(data = sd_s, idCol = "indiv", dateCol = "datetime")
+}
+realizations_conveyor_s <- map(realizations_conveyor_s, as.data.frame) # turn back to data frame
 
 # RANDOM
-# prepare simulation data for randomization method
-data.table::setDT(simulation_data)
-simulation_data$datetime <- as.POSIXct(simulation_data$datetime)
-realizations_random <- randomizations(DT = simulation_data, type = "trajectory", id = "indiv", datetime = "datetime", coords = c("x", "y"), iterations = n) %>%
+## Random: NON-SOCIABLE
+data.table::setDT(sd_ns)
+sd_ns$datetime <- as.POSIXct(sd_ns$datetime)
+realizations_random_ns <- randomizations(DT = sd_ns, type = "trajectory", id = "indiv", datetime = "datetime", coords = c("x", "y"), iterations = n) %>%
   filter(iteration != 0) %>% as.data.frame() # remove the original, since the original is `simulation_data`
-realizations_random <- realizations_random %>%
+realizations_random_ns <- realizations_random_ns %>%
+  group_split(iteration, .keep = TRUE)
+
+## Random: SOCIABLE
+data.table::setDT(sd_s)
+sd_s$datetime <- as.POSIXct(sd_s$datetime)
+realizations_random_s <- randomizations(DT = sd_s, type = "trajectory", id = "indiv", datetime = "datetime", coords = c("x", "y"), iterations = n) %>%
+  filter(iteration != 0) %>% as.data.frame() # remove the original, since the original is `simulation_data`
+realizations_random_s <- realizations_random_s %>%
   group_split(iteration, .keep = TRUE)
 
 # 4. Get stats ------------------------------------------------------------
-obs <- get_stats(data = simulation_data, edgelist = get_edgelist(data = simulation_data, idCol = "indiv", dateCol = "datetime"))
-conv_edges <- map(realizations_conveyor, ~get_edgelist(data = .x, idCol = "indiv", dateCol = "datetime"))
-conv_stats <- map2(.x = realizations_conveyor, .y = conv_edges, ~get_stats(edgelist = .y, data = .x)) %>%
+# NON-SOCIABLE
+obs_stats_ns <- get_stats(data = sd_ns, edgelist = get_edgelist(data = sd_ns, idCol = "indiv", dateCol = "datetime"))
+conv_edges_ns <- map(realizations_conveyor_ns, ~get_edgelist(data = .x, idCol = "indiv", dateCol = "datetime"))
+conv_stats_ns <- map2(.x = realizations_conveyor_ns, .y = conv_edges_ns, ~get_stats(edgelist = .y, data = .x)) %>%
   purrr::list_rbind(names_to = "iteration")
-rand_edges <- map(realizations_random, ~get_edgelist(data = .x, idCol = "indiv", dateCol = "datetime"))
-rand_stats <- map2(.x = realizations_random, .y = rand_edges, ~get_stats(edgelist = .y, data = .x)) %>%
+rand_edges_ns <- map(realizations_random_ns, ~get_edgelist(data = .x, idCol = "indiv", dateCol = "randomdatetime"))
+rand_stats_ns <- map2(.x = realizations_random_ns, .y = rand_edges_ns, ~get_stats(edgelist = .y, data = .x)) %>%
   purrr::list_rbind(names_to = "iteration")
 
-perms <- conv %>% mutate(type = "conveyor") %>% bind_rows(rand %>% mutate(type = "random"))
+perms_stats_ns <- conv_stats_ns %>% mutate(type = "conveyor") %>% bind_rows(rand_stats_ns %>% mutate(type = "random"))
+
+# SOCIABLE
+obs_stats_s <- get_stats(data = sd_s, edgelist = get_edgelist(data = sd_s, idCol = "indiv", dateCol = "datetime"))
+conv_edges_s <- map(realizations_conveyor_s, ~get_edgelist(data = .x, idCol = "indiv", dateCol = "datetime"))
+conv_stats_s <- map2(.x = realizations_conveyor_s, .y = conv_edges_s, ~get_stats(edgelist = .y, data = .x)) %>%
+  purrr::list_rbind(names_to = "iteration")
+rand_edges_s <- map(realizations_random_s, ~get_edgelist(data = .x, idCol = "indiv", dateCol = "randomdatetime"))
+rand_stats_s <- map2(.x = realizations_random_s, .y = rand_edges_s, ~get_stats(edgelist = .y, data = .x)) %>%
+  purrr::list_rbind(names_to = "iteration")
+
+perms_stats_s <- conv_stats_s %>% mutate(type = "conveyor") %>% bind_rows(rand_stats_s %>% mutate(type = "random"))
+
+# TOGETHER
+perms_stats <- perms_stats_ns %>% mutate(sociable = "non-sociable") %>% bind_rows(perms_stats_s %>% mutate(sociable = "sociable"))
+
+obs_stats <- obs_stats_ns %>% mutate(sociable = "non-sociable") %>% bind_rows(obs_stats_s %>% mutate(sociable = "sociable"))
 
 # 5. Make plot ---------------------------------------------------------------
-ord <- levels(reorder(perms$ID1, perms$degree, mean, decreasing = T))
-obs$ID1 <- factor(obs$ID1, levels = ord)
-perms %>%
+ord_deg_s <- obs_stats_s %>% arrange(desc(degree)) %>% pull(ID1)
+ord_str_s <- obs_stats_s %>% arrange(desc(strength)) %>% pull(ID1)
+ord_deg_ns <- obs_stats_ns %>% arrange(desc(degree)) %>% pull(ID1)
+ord_str_ns <- obs_stats_ns %>% arrange(desc(strength)) %>% pull(ID1)
+
+perms_stats %>%
+  mutate(ID1 = factor(ID1, levels = ord_deg_s)) %>%
   ggplot()+
-  geom_boxplot(aes(x = reorder(ID1, degree, mean, decreasing = T), y = degree, 
+  geom_boxplot(aes(x = ID1, y = degree, 
                    col = type, fill = type), position = "dodge")+
   theme_classic()+
   ylab("Degree") + xlab("Ranked agents")+
   scale_color_manual(name = "Permutation type", values = c("lightgreen", "lightblue")) + 
   scale_fill_manual(name = "Permutation type", values = c("lightgreen", "lightblue"))+
-  geom_point(data = obs, aes(x = ID1, y = degree))
+  geom_point(data = obs_stats %>% mutate(ID1 = factor(ID1, levels = ord_deg_s)), 
+             aes(x = ID1, y = degree))+
+  facet_wrap(~sociable)
 
-perms %>%
+perms_stats %>%
+  mutate(ID1 = factor(ID1, levels = ord_str_s)) %>%
   ggplot()+
-  geom_boxplot(aes(x = reorder(ID1, strength, mean, decreasing = T), y = strength, 
+  geom_boxplot(aes(x = ID1, y = strength, 
                    col = type, fill = type), position = "dodge")+
   theme_classic()+
   ylab("Strength") + xlab("Ranked agents")+
   scale_color_manual(name = "Permutation type", values = c("lightgreen", "lightblue")) + 
   scale_fill_manual(name = "Permutation type", values = c("lightgreen", "lightblue"))+
-  geom_point(data = obs, aes(x = ID1, y = strength))
-
-
-
+  geom_point(data = obs_stats %>% mutate(ID1 = factor(ID1, levels = ord_str_s)), 
+             aes(x = ID1, y = strength))+
+  facet_wrap(~sociable)
