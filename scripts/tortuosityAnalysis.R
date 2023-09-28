849b00b528af13b14f7cd54ed32b4da5da07ce88
Ryan Nguyen <ryannguyen6392@gmail.com>
2023-09-27 23:35:33 -0700

    ggplot graphs for HRRatios and sim 2 to sim 3 analysis

--- scripts/tortuosityAnalysis.R
+++ scripts/tortuosityAnalysis.R
@@ -1,20 +1,23 @@
 source("scripts/functions.R") # pulls in all the functions (written by Orr and Ryan)
 library("tidyverse")
+library("viridis")
 
 statsBySocialWeight <- list() # list of stats by social weights (0, 0.25, 0.5, 0.75)
 simsBySocialWeight <- list() # list of sims by social weights
 socialWeight <- 0 # starting social weight
 socialWeightIncrement <- 0.25 # amount to increment social weight by
-HRSteps <- 35
+# HRSteps <- 10
+baseAgentStep <- 7
+userHRRatios <- c(1, 5, 10, 25, 50, 100)
 
 for(y in 1:ceiling(1/socialWeightIncrement)){
   stats <- data.frame() # data frame to hold HR step size and mean tortuosity for the sims
-  baseHRStpSize <- 3 # starting HRStpSize
   sims <- list() # list to hold sims
-  for(x in 1:HRSteps){
-    HRStpSize <- baseHRStpSize * x
+  
+  for(x in userHRRatios){
+    HRStpSize <- baseAgentStep * x
     HRStpStd <- HRStpSize * 0.05
-    print(paste0("Working on sim: ", x, " for weight: ", socialWeight))
+    print(paste0("Working on HRRatio: ", x, " for weight: ", socialWeight))
     sim <- simulateAgents(N = 6,
                            Days = 6,
                            DayLength = 50,
@@ -24,24 +27,25 @@ for(y in 1:ceiling(1/socialWeightIncrement)){
                            Scl = 1000,
                            seed = 9252023,
                            EtaCRW = 0.7,
-                           StpSize_ind = 7,
+                           StpSize_ind = baseAgentStep,
                            StpStd_ind = 5,
                            Kappa_ind = 4,
                            ToPlot = 0,
                            quiet = T,
                            sim_3 = T,
                            socialWeight = socialWeight,
-                           HREtaCRW = 0.7,
+                           HREtaCRW = 0,
                            HRStpSize = HRStpSize,
                            HRStpStd = HRStpStd,
-                           HRKappa_ind = 4)
+                           HRKappa_ind = 0.01)
     sims <- c(sims, list(sim))
     tortuosityByIndiv <- get_tortuosity(sim$XY) # get tortuosity per individual
     mean_tortuosity <- colMeans(tortuosityByIndiv)
-    strength_tortuosity <- colSums(tortuosityByIndiv)
+    # strength_tortuosity <- colSums(tortuosityByIndiv)
+    strength_tortuosity <- 0
     stats <- rbind(stats, c(HRStpSize, mean_tortuosity, strength_tortuosity)) # add stats to list
   }
-  colnames(stats) <- c("HRStpSize", "mean_tortuosity", "strength_tortuosity") # rename columns
+  colnames(stats) <- c("HRStpSize", "mean_tortuosity", "strength_tortuos") # rename columns
   statsBySocialWeight <- c(statsBySocialWeight, list(stats)) # add stats and sims to list
   simsBySocialWeight <- c(simsBySocialWeight, list(sims))
   socialWeight <- socialWeight + socialWeightIncrement # increment social weight
@@ -53,4 +57,10 @@ lines(statsBySocialWeight[[3]]$HRStpSize, statsBySocialWeight[[3]]$mean_tortuosi
 lines(statsBySocialWeight[[4]]$HRStpSize, statsBySocialWeight[[4]]$mean_tortuosity, "l", col="purple")
 legend(legend=c("SocialWeight=0", "SocialWeight=0.25", "SocialWeight=0.5", "SocialWeight=0.75"), fill= c("green", "red", "blue", "purple"), "topright")
 
-# sim$XY %>% ggplot(aes(x = X, y = Y, col = day))+geom_point(alpha = 0.7)+geom_point(data = sim$HRCent, pch = 19, size = 5)+facet_wrap(~indiv, scales = "free")+theme_minimal()+theme(legend.position = "none")+scale_color_viridis()
+sim_0 <- simsBySocialWeight[[1]]
+for(i in 1:length(sim_0)){
+  sim <- sim_0[[i]]
+  ratio <- userHRRatios[i]
+  p <- sim$XY %>% ggplot(aes(x = X, y = Y, col = day))+geom_point(alpha = 0.7)+geom_point(data = sim$HRCent, pch = 19, size = 5)+facet_wrap(~indiv, scales = "free")+theme_minimal()+theme(legend.position = "none")+scale_color_viridis() + ggtitle(paste("Ratio", ratio))
+  ggsave(p, filename=paste0("fig/sensitivity_analysis/ratio_", ratio, ".jpg"), width=7, height = 5)
+}
