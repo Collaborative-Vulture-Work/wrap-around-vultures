8e46a804a78dc80c5e868571647e54d58798e8a4
Kaija Gahm <kgahm@ucla.edu>
2023-10-04 13:27:42 -0700

    permutations on the simulations

--- testParams.R
+++ testParams.R
@@ -5,8 +5,8 @@ HRStpSize <- baseAgentStep*r
 HRStpStd <- HRStpSize*0.75 # leaving this here for now--could go back and change later if we want. 
 hrk <- 20
 hre <- 0.7
-sim <- simulateAgents(N = 6,
-                      Days = 10,
+sim <- simulateAgents(N = 5,
+                      Days = 5,
                       DayLength = 50,
                       Soc_Percep_Rng = 1000,
                       PairedAgents = 0,
@@ -37,11 +37,11 @@ ggplot() +
   scale_color_viridis()+
   ggtitle(paste0("hrk = ", hrk, ", sw = ", socialWeight, ", ratio = ", r, ", hre = ", hre))
 
-# ggplot() + 
-#   geom_point(data = xy, aes(x = X, y = Y, col = indiv))+
-#   geom_point(data = hrcent, aes(x = X, y = Y, col = indiv), 
-#              pch = 19, size = 5)+
-#   theme(legend.position = "none", text = element_text(size = 10))+
-#   scale_color_viridis_d()+
-#   theme_minimal()+
-#   ggtitle(paste0("hrk = ", hrk, ", sw = ", socialWeight, ", ratio = ", r, ", hre = ", hre))
+ggplot() +
+  geom_point(data = xy, aes(x = X, y = Y, col = indiv))+
+  geom_point(data = hrcent, aes(x = X, y = Y, col = indiv),
+             pch = 19, size = 5)+
+  theme(legend.position = "none", text = element_text(size = 10))+
+  scale_color_viridis_d()+
+  theme_minimal()+
+  ggtitle(paste0("hrk = ", hrk, ", sw = ", socialWeight, ", ratio = ", r, ", hre = ", hre))
