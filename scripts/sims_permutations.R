bf0cc340249184070c91bea9fad920e95df44cbf
Kaija Gahm <kgahm@ucla.edu>
2023-10-04 15:19:57 -0700

    patchwork plot. trying multiple shiftMax values.

--- scripts/sims_permutations.R
+++ scripts/sims_permutations.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(tidytext)
+library(patchwork)
 source("scripts/functions.R")
 
 # Load data ---------------------------------------------------------------
@@ -36,19 +37,26 @@ sims_xy <- map(sims_xy, ~.x %>% mutate(date = lubridate::date(datetime),
 n <- 5 # how many permutations?
 
 # Conveyor permutations ---------------------------------------------------
-sm <- 5 # can shift 5 days in either direction, 10 day range total
+sms <- seq(from = 1, to = 25, by = 5)
 
-sims_conveyor <- map(sims_xy, ~{
-  realizations <- vector(mode = "list", length = n)
-  for(i in 1:n){
-    cat(".")
-    realizations[[i]] <- rotate_data_table(dataset = .x, shiftMax = sm, 
-                                           idCol = "indiv", dateCol = "date", 
-                                           timeCol = "time")
-  }
-  cat("\n")
-  return(realizations)
-})
+sms_sims_conveyor <- vector(mode = "list", length = length(sms))
+for(i in 1:length(sms)){
+  shift <- sms[i]
+  cat("shiftmax = ", shift, "\n")
+  sims_conveyor <- map(sims_xy, ~{
+    realizations <- vector(mode = "list", length = n)
+    for(j in 1:n){
+      cat(".")
+      realizations[[j]] <- rotate_data_table(dataset = .x, shiftMax = sm, 
+                                             idCol = "indiv", dateCol = "date", 
+                                             timeCol = "time")
+    }
+    cat("\n")
+    return(realizations)
+  })
+  sms_sims_conveyor[[i]] <- sims_conveyor
+}
+
 
 # Random permutations -----------------------------------------------------
 sims_xy_dt <- map(sims_xy, setDT) # make them into data tables so we can use the spatsoc function properly
@@ -79,18 +87,27 @@ obs_stats_df <- obs_stats %>% purrr::list_rbind() %>%
   mutate(uniquesim = paste(sim, sns, sep = "_"))
 
 # 2. conveyor permutations
-conveyor_stats <- vector(mode = "list", length = length(sims_conveyor))
-for(i in 1:length(sims_conveyor)){
-  edges <- map(sims_conveyor[[i]], ~get_edgelist(data = .x, idCol = "indiv", dateCol = "newdatetime"))
-  stats <- map2(.x = sims_conveyor[[i]], .y = edges, ~get_stats(edgelist = .y, data = .x)) %>%
-    purrr::list_rbind(names_to = "iteration")
-  conveyor_stats[[i]] <- stats
-}
+sms_conveyor_stats <- vector(mode = "list", length = length(sms))
+for(i in 1:length(sms)){
+  conveyor_stats <- vector(mode = "list", length = length(sims_conveyor))
+  for(j in 1:length(sims_conveyor)){
+    edges <- map(sims_conveyor[[j]], ~get_edgelist(data = .x, idCol = "indiv", dateCol = "newdatetime"))
+    stats <- map2(.x = sims_conveyor[[j]], .y = edges, ~get_stats(edgelist = .y, data = .x)) %>%
+      purrr::list_rbind(names_to = "iteration")
+    conveyor_stats[[j]] <- stats
+  }
+  
+  # Label the stats
+  conveyor_stats <- map2(conveyor_stats, simulations, ~.x %>% mutate(sim = .y))
+  conveyor_stats <- map2(conveyor_stats, soc_nonsoc, ~.x %>% mutate(sns = .y))
+  conveyor_stats_df <- conveyor_stats %>% purrr::list_rbind()
+  sms_conveyor_stats[[i]] <- conveyor_stats_df
+} 
 
-# Label the stats
-conveyor_stats <- map2(conveyor_stats, simulations, ~.x %>% mutate(sim = .y))
-conveyor_stats <- map2(conveyor_stats, soc_nonsoc, ~.x %>% mutate(sns = .y))
-conveyor_stats_df <- conveyor_stats %>% purrr::list_rbind()
+sms_conveyor_stats_df <- sms_conveyor_stats %>% 
+  map2(.x = ., .y = sms, ~.x %>% 
+         mutate(shiftMax = .y)) %>%
+  purrr::list_rbind()
                   
 # 3. random permutations
 random_stats <- vector(mode = "list", length = length(sims_random))
@@ -107,87 +124,61 @@ random_stats <- map2(random_stats, soc_nonsoc, ~.x %>% mutate(sns = .y))
 random_stats_df <- random_stats %>% purrr::list_rbind()
 
 # Combine all the stats
-stats_perm <- conveyor_stats_df %>% mutate(type = "conveyor") %>%
+stats_perm <- sms_conveyor_stats_df %>% mutate(type = "conveyor") %>%
   bind_rows(random_stats_df %>% mutate(type = "random")) %>%
   mutate(uniquesim = paste(sim, sns, sep = "_"))
 
-# Plots -------------------------------------------------------------------
-test <- conveyor_stats_df %>%
-  group_by(ID1, sim, sns) %>%
-  summarize(mnstr = mean(strength, na.rm = T)) %>%
-  ungroup() %>%
-  group_by(sim, sns) %>%
-  arrange(desc(mnstr), .by_group = T) %>%
-  mutate(rank = 1:n()) %>%
-  select(-mnstr)
-
-sp_test <- left_join(stats_perm, test, by = c("ID1", "sim", "sns"))
-obs_test <- left_join(obs_stats_df, test, by = c("ID1", "sim", "sns")) %>%
-  mutate(uniquesim = paste(sim, sns, sep = "_")) %>%
-  mutate(uniquesim = as.factor(uniquesim),
-         ID1 = reorder_within(ID1, rank, uniquesim))
-
-sp_test %>%
-  mutate(uniquesim = paste(sim, sns, sep = "_")) %>%
-  mutate(uniquesim = as.factor(uniquesim),
-         ID1 = reorder_within(ID1, rank, uniquesim)) %>%
-  ggplot()+
-  geom_boxplot(aes(x = ID1, y = strength, col = type, fill = type), position = "dodge")+
-  #geom_point(data = obs_stats_df, aes(x = ID1, y = strength))+
-  facet_wrap(~uniquesim, scales = "free", nrow = 3, ncol = 2)+
-  scale_x_reordered()+
-  theme_classic()
-
 # 5. Make plot ---------------------------------------------------------------
-
-for(i in 1:length(unique(obs_stats_df$uniquesim))){
-  us <- unique(obs_stats_df$uniquesim)[i]
-  datobs <- obs_stats_df %>% filter(uniquesim == us)
-  datperm <- stats_perm %>% filter(uniquesim == us)
-  dat <- bind_rows(datobs, datperm)
-  dat %>%
-    group_by(ID1, type) %>%
-    arrange(desc(degree), .by_group = T) %>%
-    
+# 6-panel boxplot
+uniquesims <- unique(obs_stats_df$uniquesim)
+plots <- vector(mode = "list", length = length(uniquesims))
+for(i in 1:length(uniquesims)){
+  usim <- unique(obs_stats_df$uniquesim)[i]
+  datobs <- obs_stats_df %>% filter(uniquesim == usim) %>% as.data.frame()
+  
+  # Get the order for the x axis (based on observed), and arrange both observed and simulated to adhere to this order.
+  ord <- datobs %>%
+    arrange(desc(strength)) %>%
+    pull(ID1)
+  datobs <- datobs %>%
+    mutate(ID1 = factor(ID1, levels = ord))
+  datperm <- stats_perm %>%
+    filter(uniquesim == usim) %>%
+    mutate(ID1 = factor(ID1, levels = ord))
+  
+  # Make the plot
+  p <- datperm %>%
+    filter(!is.na(ID1)) %>% # XXX need to deal with NA values for ID1--they didn't have stats calculated for them
+    ggplot()+
+    geom_boxplot(aes(x = ID1, y = strength, col = type, fill = type), position = position_dodge())+
+    theme_classic()+
+    geom_point(data = datobs, aes(x = ID1, y = strength), col = "black")+
+    theme(axis.ticks.x = element_blank(),
+          axis.text.x = element_blank(),
+          legend.position = "none",
+          axis.title = element_blank())+
+    ggtitle(usim)
+  plots[[i]] <- p
 }
 
+test <- patchwork::wrap_plots(plots[[1]], plots[[2]], plots[[3]], plots[[4]], plots[[5]], plots[[6]], ncol = 2, byrow = T)
+gt <- patchwork::patchworkGrob(test)
+gridExtra::grid.arrange(gt, left = "Strength", bottom = "Ranked agents")
 
+# Histograms: mean value vs. black
+summ <- stats_perm %>%
+  group_by(uniquesim, type, iteration) %>%
+  summarize(mndeg = mean(degree, na.rm = T),
+            mnstr = mean(strength, na.rm = T))
+obs_summ <- obs_stats_df %>%
+  group_by(uniquesim) %>%
+  summarize(mndeg = mean(degree, na.rm = T),
+            mnstr = mean(strength, na.rm = T))
 
-
-ord_deg_s <- obs_stats_s %>% arrange(desc(degree)) %>% pull(ID1)
-ord_str_s <- obs_stats_s %>% arrange(desc(strength)) %>% pull(ID1)
-ord_deg_ns <- obs_stats_ns %>% arrange(desc(degree)) %>% pull(ID1)
-ord_str_ns <- obs_stats_ns %>% arrange(desc(strength)) %>% pull(ID1)
-
-deg_boxplot <- perms_stats %>%
-  mutate(ID1 = factor(ID1, levels = ord_deg_s)) %>%
-  ggplot()+
-  geom_boxplot(aes(x = ID1, y = degree, 
-                   col = type, fill = type), position = "dodge")+
+summ %>%
+  ggplot(aes(x = mnstr, col = type, fill = type))+
+  geom_histogram()+
+  facet_wrap(~uniquesim, ncol = 2, nrow = 3)+
   theme_classic()+
-  ylab("Degree") + xlab("Ranked agents")+
-  scale_color_manual(name = "Permutation type", values = c("yellowgreen", "skyblue")) + 
-  scale_fill_manual(name = "Permutation type", values = c("yellowgreen", "skyblue"))+
-  geom_point(data = obs_stats %>% mutate(ID1 = factor(ID1, levels = ord_deg_s)), 
-             aes(x = ID1, y = degree))+
-  facet_wrap(~sociable)+
-  theme(text = element_text(size = 16), axis.text.x = element_blank(), axis.ticks.x = element_blank())
-deg_boxplot
-ggsave(deg_boxplot, file = "fig/degree_boxplot.png", width = 7, height = 4)
-
-str_boxplot <- perms_stats %>%
-  mutate(ID1 = factor(ID1, levels = ord_str_s)) %>%
-  ggplot()+
-  geom_boxplot(aes(x = ID1, y = strength, 
-                   col = type, fill = type), position = "dodge")+
-  theme_classic()+
-  ylab("Strength") + xlab("Ranked agents")+
-  scale_color_manual(name = "Permutation type", values = c("yellowgreen", "skyblue")) + 
-  scale_fill_manual(name = "Permutation type", values = c("yellowgreen", "skyblue"))+
-  geom_point(data = obs_stats %>% mutate(ID1 = factor(ID1, levels = ord_str_s)), 
-             aes(x = ID1, y = strength))+
-  facet_wrap(~sociable)+
-  theme(text = element_text(size = 16), axis.text.x = element_blank(), axis.ticks.x = element_blank())
-str_boxplot
-ggsave(str_boxplot, file = "fig/strength_boxplot.png", width = 7, height = 4)
-
+  theme(legend.position = "none")+
+  geom_vline(data = obs_summ, aes(xintercept = mnstr), linetype = 2)
