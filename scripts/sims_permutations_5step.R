91faf5cf12d3d200b388ca3f7c5bdad8caec6e80
Kaija Gahm <kgahm@ucla.edu>
2023-10-12 09:38:43 -0700

    5-step permutations with 50 reps

--- scripts/sims_permutations_5step.R
+++ scripts/sims_permutations_5step.R
@@ -34,10 +34,10 @@ sims_xy_5step <- map(sims_xy_5step, ~.x %>% mutate(date = lubridate::date(dateti
 
 
 # PERMUTATIONS ------------------------------------------------------------
-n <- 25 # how many permutations?
+n <- 50 # how many permutations?
 
 # Conveyor permutations ---------------------------------------------------
-sms <- seq(from = 1, to = 25, by = 2)
+sms <- seq(from = 1, to = 25, by = 1)
 
 sms_sims_conveyor_5step <- vector(mode = "list", length = length(sms))
 for(i in 1:length(sms)){
